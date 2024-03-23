f617f1c24d3525bf0fe56d8b388a1f9810e15599
sarbasali <sarbasali8@gmail.com>
2024-03-23 13:44:24 +0530

    MyNikah

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/auth/login/login.component';
+import { SignUpComponent } from './component/auth/sign-up/sign-up.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
