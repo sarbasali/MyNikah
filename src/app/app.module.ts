190a8545759b26f47aa5136521ddc82b10b5375f
sarbasali <sarbasali8@gmail.com>
2024-05-25 13:00:27 +0530

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,11 +8,17 @@ import { LoginComponent } from './component/auth/login/login.component';
 import { SignUpComponent } from './component/auth/sign-up/sign-up.component';
 import { HomeComponent } from './component/home/home.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
-// import { RegisterComponent } from './component/register/register.component';
 import { RegisterComponent } from './component/register/register.component';
-import {MatTabsModule} from '@angular/material/tabs';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+// import {MatInputModule} from '@angular/material/input';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -26,15 +33,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatTabsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule
     
 
   ],
   providers: [
-    // provideAnimationsAsync()
-  
-    provideAnimationsAsync()
+    provideNativeDateAdapter()
   ],
   bootstrap: [AppComponent]
 })
--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-
+import {FormBuilder ,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
-  // standalone: true,
- 
-  // exports: [RegisterComponent]
+
 })
+
 export class RegisterComponent {
+ 
+ 
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = true;
+
+
+  religion = 'none';
+  nationality='none'
+  constructor(private _formBuilder: FormBuilder) {
+   
+  }
+
 }
